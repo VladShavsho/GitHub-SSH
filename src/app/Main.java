d726f7f467a175d4743c3df6b2a89d64ba39f9d5
VladislavSCH <114906748+VladislavSCH@users.noreply.github.com>
2025-02-05 11:25:27 +0200

    Add conversion of miles to kilometers

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,4 +1,17 @@
 package app;
 
 public class Main {
+    public static void main(String[] args) {
+        System.out.println("Converter v1.0");
+
+        double mils = 10;
+        double kilometers = milesToKilometers(mils);
+        System.out.println("Result is " + mils + " миль це " + kilometers + " кілометрів");
+
+    }
+
+    public static double milesToKilometers(double mils) {
+        return mils  * 1.60934;
+    }
+
 }
